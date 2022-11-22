a5cc5fda730f81ef42eb31309a9b023a52dc7217
Luis <luisvillarreal.lgvc@hotmail.com>
2022-11-21 21:44:47 -0600

    Set up user data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,16 +14,33 @@ class UserController extends Controller{
 
     //Get data from the configuration form
     public function update(Request $request){
-    	$id = \Auth::user()->id;
+    	//Get user auth data
+    	$user = \Auth::user();
+    	$id = $user->id;
+
+    	//Validate data
     	$validate = $this->validate($request, [
     		'name' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
             'nick' => ['required', 'string', 'max:20', Rule::unique('users')->ignore($id)],
             'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($id)]
     	]);
+
+    	//Get user data
     	$name = $request->input('name');
     	$surname = $request->input('surname');
     	$nick = $request->input('nick');
     	$email = $request->input('email');
+
+    	//Assign data to new object and update
+    	$user->name = $name;
+    	$user->surname = $surname;
+    	$user->nick = $nick;
+    	$user->email = $email;
+    	$user->update();
+
+    	//Redirect
+    	return redirect()->route('config')
+    					 ->with(['message' => 'User successfully updated']);
     }
 }
