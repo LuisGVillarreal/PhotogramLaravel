7e21f7f33dd0122686829327513411773a080c59
Luis <luisvillarreal.lgvc@hotmail.com>
2022-11-27 22:51:57 -0600

    Action dislike

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -6,31 +6,50 @@ use Illuminate\Http\Request;
 use App\Like;
 
 class LikeController extends Controller{
-    //Restrict only to authenticated users
+	//Restrict only to authenticated users
 	public function __construct(){
-        $this->middleware('auth');
-    }
+		$this->middleware('auth');
+	}
 
-    //
-    public function like($image_id){
-    	$user =  \Auth::user();
+	//like
+	public function like($image_id){
+		$user =  \Auth::user();
 
-    	//Check if the "Like" exists & don't duplicate it
-    	$isset_like = Like::where('user_id',$user->id)->where('image_id',$image_id)->count();
-    	if ($isset_like == 0) {
-    		$like = new Like();
-	    	$like->user_id = $user->id;
-	    	$like->image_id = (int)$image_id;
+		//Check if the "Like" exists & don't duplicate it
+		$isset_like = Like::where('user_id',$user->id)->where('image_id',$image_id)->count();
+		if ($isset_like == 0) {
+			$like = new Like();
+			$like->user_id = $user->id;
+			$like->image_id = (int)$image_id;
 
-	    	//Save like
-	    	$like->save();
-	    	return response()->json([
-	    		'like' => $like
-	    	]);
-    	} else {
-    		return response()->json([
-	    		'message' => 'you have already given a like'
-	    	]);
-    	}
-    }
+			//Save like
+			$like->save();
+			return response()->json([
+				'like' => $like
+			]);
+		} else {
+			return response()->json([
+				'message' => 'you have already given a like'
+			]);
+		}
+	}
+
+	//dislike
+	public function dislike($image_id){
+		$user =  \Auth::user();
+
+		//Check if the "Like" exists & don't duplicate it
+		$like = Like::where('user_id',$user->id)->where('image_id',$image_id)->first();
+		if ($like) {
+			$like->delete();
+			return response()->json([
+				'like' => $like,
+				'message' => 'You have given a dislike'
+			]);
+		} else {
+			return response()->json([
+				'message' => 'Error'
+			]);
+		}
+	}
 }
