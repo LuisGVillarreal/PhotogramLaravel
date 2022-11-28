c994a967f22dbb0b6cebf578b4a3b2d233d885b2
Luis <luisvillarreal.lgvc@hotmail.com>
2022-11-28 01:02:00 -0600

    Like function in detail

--- resources/views/image/detail.blade.php
+++ resources/views/image/detail.blade.php
@@ -20,7 +20,19 @@
 						<strong>{{ '@'.$image->user->nick }}</strong>
 						{{ ' | '.\FormatTime::LongTimeFilter($image->created_at) }}
 						<p class="m-0 mb-1">{{ $image->description }}</p>
-						<button type="button" class="btn btn-outline-danger"><i class="bi bi-heart"></i>&nbsp;Like</button>
+
+						@php $user_like = false; @endphp
+						@foreach ($image->likes as $like)
+							@if ($like->user->id == Auth::user()->id)
+								@php $user_like = true; @endphp
+							@endif
+						@endforeach
+						@if ($user_like)
+							<button type="button" class="btn btn-dislike btn-danger" data-id="{{ $image->id }}"><i class="bi bi-heart"></i>&nbsp;Like&nbsp;({{ count($image->likes) }})</button>
+						@else
+							<button type="button" class="btn btn-like btn-outline-danger" data-id="{{ $image->id }}"><i class="bi bi-heart"></i>&nbsp;Like&nbsp;({{ count($image->likes) }})</button>
+						@endif
+
 						<button class="btn btn-primary ml-md-2" type="button" disabled><i class="bi bi-chat"></i>&nbsp;Comments&nbsp;({{ count($image->comments) }})</button>
 						<hr>
 						<form class="row mt-3" method="post" action="{{ route('comment.save') }}">
