d5f6a9c4f2912709e77ec117ed21ae022c80fed4
Luis <luisvillarreal.lgvc@hotmail.com>
2022-12-02 00:12:42 -0600

    Show user data in the profile

--- resources/views/include/simpleImage.blade.php
+++ resources/views/include/simpleImage.blade.php
@@ -1,22 +1,9 @@
-<div class="card mb-3">
-	<div class="card-header d-flex align-items-center">
-		@if ($image->user->avatar)
-			<img src="{{ route('user.avatar',['filename'=>$image->user->avatar]) }}" alt="mdo" width="32" height="32" class="rounded-circle align-middle">
-		@endif
-		<a href="{{ route('profile', ['id' => $image->user->id]) }}" class="link-dark">
-			<strong class="ml-2 text-dark">{{ $image->user->name.' '.$image->user->surname }}</strong>
-			<span class="text-muted ml-1">{{ '@'.$image->user->nick }}</span>
-		</a>&nbsp;
-			{{ ' · '.\FormatTime::LongTimeFilter($image->created_at) }}
-	</div>
-
-	<div class="card-body p-0">
+<div class="col mb-4">
+	<div class="card">
 		<a href="{{ route('image.detail',['id'=>$image->id]) }}">
-			<img src="{{ route('image.file',['filename'=>$image->image_path]) }}" class="img-fluid"></a>
-		</div>
-		<div class="card-footer">
-			<p class="m-0 mb-1">{{ $image->description }}</p>
-
+			<img src="{{ route('image.file',['filename'=>$image->image_path]) }}" class="img-fluid">
+		</a>
+		  <div class="card-footer">
 			@php $user_like = false; @endphp
 			@foreach ($image->likes as $like)
 				@if ($like->user->id == Auth::user()->id)
@@ -32,3 +19,4 @@
 			<a href="{{ route('image.detail',['id'=>$image->id]) }}" class="btn btn-primary ml-md-2" type="button"><i class="bi bi-chat"></i>&nbsp;Comments&nbsp;({{ count($image->comments) }})</a>
 		</div>
 	</div>
+</div>
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,11 +4,27 @@
 <div class="container">
 	<div class="row justify-content-center">
 		<div class="col-md-8">
-			<h1>My Profile</h1>
+			<div class="d-flex">
+				<div class="avatar mx-5">
+					@if ($user->avatar)
+						<img src="{{ route('user.avatar',['filename'=>$user->avatar]) }}" alt="mdo" width="150" height="150" class="rounded-circle align-middle">
+					@endif
+				</div>
+				<div class="info">
+					<h2>{{ '@'.$user->nick }}</h2>
+					<p class="lead"><strong>{{ count($user->images) }}</strong> posts</p>
+					<p class="">
+						<strong>{{ $user->name.' '.$user->surname }}</strong><br>
+						{{ 'Joined '.\FormatTime::LongTimeFilter($user->created_at) }}
+					</p>
+				</div>
+			</div>
 			<hr>
-			@foreach ($user->images as $image)
-				@include('include.image', ['image' => $image])
-			@endforeach
+			<div class="row row-cols-1 row-cols-md-2">
+				@foreach ($user->images as $image)
+					@include('include.simpleImage', ['image' => $image])
+				@endforeach
+			</div>
 		</div>
 	</div>
 </div>
